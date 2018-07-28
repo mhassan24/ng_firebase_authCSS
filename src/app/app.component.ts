a260ae03c5e136917ff130656d51b61115bc2f5d
name <u@example.com>
2018-07-28 11:27:40 +0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  email: string;
+  password: string;
+  // title = 'app';
+
+  constructor(public authService: AuthService) {}
+
+  signup() {
+    this.authService.signup(this.email, this.password);
+    this.email = this.password = '';
+  }
+
+  login() {
+    this.authService.login(this.email, this.password);
+    this.email = this.password = '';    
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
